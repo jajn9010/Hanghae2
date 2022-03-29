adefdf3b38fdd30db444b79dc11f5903fd2af924
JaejinLee <jaejin1112@gmail.com>
2022-03-29 21:49:07 +0900

    test commit

--- src/main/java/com/sparta/expert_mission/controller/BlogController.java
+++ src/main/java/com/sparta/expert_mission/controller/BlogController.java
@@ -3,26 +3,37 @@ package com.sparta.expert_mission.controller;
 import com.sparta.expert_mission.model.Blog;
 import com.sparta.expert_mission.repository.BlogRepository;
 import com.sparta.expert_mission.dto.BlogRequestDto;
+import com.sparta.expert_mission.security.UserDetailsImpl;
 import com.sparta.expert_mission.service.BlogService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class BlogController {
 
     private final BlogRepository blogRepository;
     private final BlogService blogService;
 
     @GetMapping("/api/blogs")
+    @ResponseBody
     public List<Blog> getBlogs() {
         return blogRepository.findAllByOrderByModifiedAtDesc();
     }
 
-    @PostMapping("/api/blogs")
-    public Blog createBlog(@RequestBody BlogRequestDto requestDto) {
+    @GetMapping("/api/blogs/write")
+    public String write() {
+        return "write";
+    }
+
+    @PostMapping("/api/blogs/write")
+    @ResponseBody
+    public Blog createBlog(@RequestBody BlogRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        requestDto.setUsername(userDetails.getUsername());
         Blog blog = new Blog(requestDto);
         return blogRepository.save(blog);
     }
@@ -38,4 +49,5 @@ public class BlogController {
         blogRepository.deleteById(id);
         return id;
     }
+
 }
--- src/main/java/com/sparta/expert_mission/dto/BlogRequestDto.java
+++ src/main/java/com/sparta/expert_mission/dto/BlogRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.expert_mission.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class BlogRequestDto {
     private Long userId;
     private String username;
