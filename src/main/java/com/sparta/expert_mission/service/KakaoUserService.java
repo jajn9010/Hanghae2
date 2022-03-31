ae92dadbfb0010ec2bd0bf7de7c898f986683f45
JaejinLee <jaejin1112@gmail.com>
2022-03-31 21:09:34 +0900

    change SQL

--- src/main/java/com/sparta/expert_mission/service/KakaoUserService.java
+++ src/main/java/com/sparta/expert_mission/service/KakaoUserService.java
@@ -58,7 +58,7 @@ public class KakaoUserService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "7c3596078eced38e9e1639b636215976");
-        body.add("redirect_uri", "http://localhost:8080/user/kakao/callback");
+        body.add("redirect_uri", "http://13.124.230.105/user/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
